056ca15d94ffd61500aa86793b872fc4880c0b81
masudur-123 <masudurahaman555@gmail.com>
2025-05-24 11:32:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,58 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import streamlit as st
 import numpy as np
+import matplotlib.image as mpimg
 
-# Read in the image
-image = mpimg.imread('laneLines_thirdPass.jpg')
+st.title("Road Lane Line Detection")
 
-# Grab the x and y size and make a copy of the image
-ysize = image.shape[0]
-xsize = image.shape[1]
-color_select = np.copy(image)
-line_image = np.copy(image)
+uploaded_file = st.file_uploader("Upload a road image (jpg, png)", type=["jpg", "jpeg", "png"])
 
-# Define color selection criteria
-# MODIFY THESE VARIABLES TO MAKE YOUR COLOR SELECTION
-red_threshold = 200
-green_threshold = 200
-blue_threshold = 200
+if uploaded_file is not None:
+    # Read image as numpy array
+    image = mpimg.imread(uploaded_file)
 
-rgb_threshold = [red_threshold, green_threshold, blue_threshold]
+    ysize = image.shape[0]
+    xsize = image.shape[1]
+    color_select = np.copy(image)
+    line_image = np.copy(image)
 
-# Define the vertices of a triangular mask.
-# Keep in mind the origin (x=0, y=0) is in the upper left
-# MODIFY THESE VALUES TO ISOLATE THE REGION
-# WHERE THE LANE LINES ARE IN THE IMAGE
-left_bottom = [100, 539]
-right_bottom = [950, 539]
-apex = [480, 290]
+    # Color thresholds (tweak if needed)
+    red_threshold = 200
+    green_threshold = 200
+    blue_threshold = 200
+    rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 
-# Perform a linear fit (y=Ax+B) to each of the three sides of the triangle
-# np.polyfit returns the coefficients [A, B] of the fit
-fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
-fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
-fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
+    # Define region of interest polygon (triangle)
+    left_bottom = [100, ysize - 1]
+    right_bottom = [xsize - 100, ysize - 1]
+    apex = [xsize // 2, ysize // 2]
 
-# Mask pixels below the threshold
-color_thresholds = (image[:,:,0] < rgb_threshold[0]) | \
-                    (image[:,:,1] < rgb_threshold[1]) | \
-                    (image[:,:,2] < rgb_threshold[2])
+    # Fit lines for the triangular region
+    fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
+    fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
+    fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
 
-# Find the region inside the lines
-XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
-region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
-                    (YY > (XX*fit_right[0] + fit_right[1])) & \
-                    (YY < (XX*fit_bottom[0] + fit_bottom[1]))
+    # Color threshold mask
+    color_thresholds = (image[:,:,0] < rgb_threshold[0]) | \
+                       (image[:,:,1] < rgb_threshold[1]) | \
+                       (image[:,:,2] < rgb_threshold[2])
 
-# Mask color and region selection
-color_select[color_thresholds | ~region_thresholds] = [0, 0, 0]
-# Color pixels red where both color and region selections met
-line_image[~color_thresholds & region_thresholds] = [9, 255, 0]
+    # Region mask
+    XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
+    region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
+                        (YY > (XX*fit_right[0] + fit_right[1])) & \
+                        (YY < (XX*fit_bottom[0] + fit_bottom[1]))
 
-# Display the image and show region and color selections
-plt.imshow(image)
-x = [left_bottom[0], right_bottom[0], apex[0], left_bottom[0]]
-y = [left_bottom[1], right_bottom[1], apex[1], left_bottom[1]]
-plt.plot(x, y, 'r--', lw=4)
-plt.title("Region Of Interest")
-plt.show()
-plt.imshow(color_select)
-plt.title("Color Selection")
-plt.show()
-plt.imshow(line_image)
-plt.title("Output Image")
-plt.show()
+    # Mask pixels outside thresholds
+    color_select[color_thresholds | ~region_thresholds] = [0, 0, 0]
+
+    # Highlight lane lines in bright green
+    line_image[~color_thresholds & region_thresholds] = [9, 255, 0]
+
+    # Show images in Streamlit
+    st.image(image, caption="Original Image", use_column_width=True)
+    st.image(color_select, caption="Color Selection", use_column_width=True)
+    st.image(line_image, caption="Lane Lines Detected", use_column_width=True)
+
+else:
+    st.write("Please upload an image to start lane line detection.")
+
